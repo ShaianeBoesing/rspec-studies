cddb96967b1317bea4f58820c7ce8edf13df01ed
Shaiane Boesing <shaianeboesingrb@gmail.com>
2022-09-28 16:02:01 -0300

    :sparkles: feat: arrays matchers

---
+++ spec/matchers/arrays_spec.rb
@@ -0,0 +1,12 @@
+RSpec.describe [1,2,3], "Array" do 
+    it "#include" do 
+        expect(subject).to include(2,1)
+    end
+    it "#match_array"  do #alias of #contain_exactly 
+        expect(subject).to match_array([1,3,2])
+    end
+
+    it "#contain_exactly" do
+        expect(subject).to contain_exactly(3,1,2)
+    end
+end
