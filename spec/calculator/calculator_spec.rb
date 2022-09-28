76a6d0bb11e06d844c9c3918c8685c022e9ee473
Shaiane Boesing <shaianeboesingrb@gmail.com>
2022-09-28 16:02:35 -0300

    :sparkles: feat: exception matchers

--- spec/calculator/calculator_spec.rb
+++ spec/calculator/calculator_spec.rb
@@ -17,4 +17,12 @@ describe Calculator do
 			expect(result).to eq(-1)
 		end
 	end
+
+	context '#div' do
+		it "must divide by zero and throws an error" do
+			expect{subject.div(3, 0)}.to raise_error(ZeroDivisionError)
+			expect{subject.div(3, 0)}.to raise_error("divided by 0")
+			expect{subject.div(3, 0)}.to raise_error(ZeroDivisionError, "divided by 0")
+		end
+	end
 end
