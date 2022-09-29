3d0b6a2af5127c9fc5982152311a71cdfc70109a
Shaiane Boesing <shaianeboesingrb@gmail.com>
2022-09-29 15:19:45 -0300

    :sparkles: feat: Helper and Hooks (Before, After and Around)

--- spec/matchers/attributes_spec.rb
+++ spec/matchers/attributes_spec.rb
@@ -1,13 +1,17 @@
 require 'person'
 # alias at https://gist.github.com/junichiito/f603d3fbfcf99b914f86
 describe 'Person Attributes' do 
-    it "have_attributes" do 
-        person1 = Person.new
-        person1.name = "Shaiane"
-        person1.age = 20
-        person2 = Person.new
 
-        expect(person1).to have_attributes(:name => starting_with('Shai'), :age => (be >= 18))
-        expect(person2).to have_attributes(:name => nil)
+    before(:each) do #alias for example
+        @person = Person.new
+        @person2 = Person.new
+    end
+
+    it "have_attributes" do 
+        @person.name = "Shaiane"
+        @person.age = 20
+
+        expect(@person).to have_attributes(:name => starting_with('Shai'), :age => (be >= 18))
+        expect(@person2).to have_attributes(:name => nil)
     end
 end 
