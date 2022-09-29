43a3d39aeea9a02cfffe21c529035fc66af85540
Shaiane Boesing <shaianeboesingrb@gmail.com>
2022-09-29 15:55:09 -0300

    :sparkles: feat: using let to create vars

--- spec/matchers/attributes_spec.rb
+++ spec/matchers/attributes_spec.rb
@@ -1,17 +1,15 @@
 require 'person'
 # alias at https://gist.github.com/junichiito/f603d3fbfcf99b914f86
 describe 'Person Attributes' do 
-
-    before(:each) do #alias for example
-        @person = Person.new
-        @person2 = Person.new
-    end
+    # replaces use of before(:each) to create a instance of Person
+    let(:person) { Person.new }
+    let(:person2) { Person.new }
 
     it "have_attributes" do 
-        @person.name = "Shaiane"
-        @person.age = 20
+        person.name = "Shaiane"
+        person.age = 20
 
-        expect(@person).to have_attributes(:name => starting_with('Shai'), :age => (be >= 18))
-        expect(@person2).to have_attributes(:name => nil)
+        expect(person).to have_attributes(:name => starting_with('Shai'), :age => (be >= 18))
+        expect(person2).to have_attributes(:name => nil)
     end
 end 
