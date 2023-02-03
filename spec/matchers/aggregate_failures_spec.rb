fc476e6a44f80fc050db92f46432e5a861fabe5d
Shaiane Boesing <shaianeboesingrb@gmail.com>
2023-02-02 21:37:36 -0300

    :sparkles: feat: aggregate failures matcher second way

--- spec/matchers/aggregate_failures_spec.rb
+++ spec/matchers/aggregate_failures_spec.rb
@@ -7,4 +7,10 @@ describe [10,11,12].sample do
     end
   end
 
+  it 'Aggregate Failures 2', :aggregate_failures do 
+    is_expected.to eq(10)
+    is_expected.to eq(11)
+    is_expected.to eq(12)  
+  end
+
 end
